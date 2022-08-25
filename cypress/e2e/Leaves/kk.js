6d4f8fd95ef90cfd03c751518dab35e5a13e827c
Shahbaz <shahshahbaz64@gmail.com>
2022-08-25 08:42:20 +0500

    liniting check

--- cypress/e2e/Leaves/kk.js
+++ cypress/e2e/Leaves/kk.js
@@ -2,6 +2,9 @@ describe("ABC", () => {
   it("abc", () => {
     cy.visit("https://example.cypress.io");
 
-    cy.log("Abc     ");
+    cy.log("Abc");
+    cy.log(
+      "ut also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh"
+    );
   });
 });
